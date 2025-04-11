ee244877cd592d6e63431d18565b26af4118f959
Felix-Think <felixthink.ai@gmail.com>
2025-04-11 17:15:53 +0700

    Hoan Thien Register va ViT

--- Register.py
+++ Register.py
@@ -5,6 +5,7 @@ import cv2
 import os
 from TakeImage import TakeImage
 from PIL import Image, ImageTk
+from ViT_Recognition import ViTFaceRecognition
 
 class Register:
     def __init__(self, root, main_ui):
@@ -106,6 +107,9 @@ class Register:
             print(f"Lỗi: {e}")
 
         self.open_TakeImage(ID)
+        # Hien thi anh cuoi cung cua ID
+        self.get_img()
+
     
     def get_img(self):
         img_id = self.entry_id.get()
@@ -127,6 +131,18 @@ class Register:
     def open_TakeImage(self,ID):
         self.root.withdraw()
         TakeImage(self.root,ID)
+
+    def train_new_user(self, ID):
+        label = ID
+        src_dir = os.path.join('Datasets', ID)
+        vit = ViTFaceRecognition(src_dir=src_dir)
+        images, labels = vit.load_data(train=False, faiss_index_path='faiss_index.faiss', metadata_path='faiss_index_metadata.pkl')
+        #features = vit.extract_features(images)
+        #vit.build_and_save_faiss_index(features, save_path='faiss_index') if file is not exist, you have to train index for images
+        # add new user
+        paths_new_user = os.listdir(src_dir)
+        usr_paths = [os.path.join(src_dir, path) for path in paths_new_user] # Lay duong dan de cac anh cua ID 
+        vit.add_new_user(usr_paths, label, faiss_index_path='faiss_index.faiss', metadata_path='faiss_index_metadata.pkl')
 
     def on_close(self):
         """Handle the window close event."""
